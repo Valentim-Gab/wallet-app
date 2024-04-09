fab055d15bc35a12c6528aea6ed5ee28237adbd1
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2024-04-09 17:59:21 -0300

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ components/Button.tsx
@@ -0,0 +1,11 @@
+import { forwardRef } from 'react';
+
+import { Button as TButton, ButtonText } from '../tamagui.config';
+
+export const Button = forwardRef<TouchableOpacity, ButtonProps>(({ onPress, title }, ref) => {
+  return (
+    <TButton onPress={onPress}>
+      <ButtonText>{title}</ButtonText>
+    </TButton>
+  );
+});
